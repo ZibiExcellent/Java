0ef115fab046690c8fd550b431717456a54f38ab
Zibi <zbigniew.excellent@gmail.com>
2025-02-05 20:33:54 +0100

    pierwsza Java na repo Java

---
+++ TestGitHub.java
@@ -0,0 +1,8 @@
+public class TestGitHub{
+    public static void main(String[] args) {
+        System.out.println("Pierwszy mój plik na GitHub");
+        System.out.println("Sporo musiałem włożyć pracy by rozgryźć Git i Github, ;-)" +
+                "\njednak jeśli to czytasz, to musiało mi się jednak udać." +
+                "\nPrzede mną jeszcze zrozumienie 'gałęzi' w Git i będzie chyba oki toki.");
+    }
+}
